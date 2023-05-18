5badf07920788467d853e6c72ec797ae924d88c3
YeongGi <yeonggilol@gmail.com>
2023-05-18 23:31:50 +0900

    feat: add easing and text division styles

--- src/app/text/page.tsx
+++ src/app/text/page.tsx
@@ -6,11 +6,26 @@ import IPhoneX from "../components/IPhoneX";
 import { gsapOptions } from "./gsapOptions";
 import GsapSlider from "../components/Sliders";
 import { useDebounce } from "@toss/react";
-import { Button } from "@mui/joy";
+import { Button, Select, Option } from "@mui/joy";
+
+type TextStyle = "words" | "chars" | "lines";
+type EasingType =
+  | "power0"
+  | "power1"
+  | "power2"
+  | "power3"
+  | "power4"
+  | "back"
+  | "elastic"
+  | "bounce"
+  | "rough"
+  | "expo";
 
 export default function Text() {
-  const [counter, setCounter] = useState(0);
   const textRef = useRef<HTMLElement[] | null>(null);
+  const [counter, setCounter] = useState(0);
+  const [textStyle, setTextStyle] = useState<TextStyle>("words");
+  const [easing, setEasing] = useState<EasingType>("back" as EasingType);
   const [gsapStates, setGsapStates] = useState(
     gsapOptions.reduce((acc, cur) => {
       acc[cur.type] = cur.default;
@@ -24,15 +39,15 @@ export default function Text() {
         setGsapStates({ ...gsapStates, [type]: newValue });
     };
 
-  useEffect(() => {
-    const text = new SplitType("div.text1");
-    const words = text.words;
-    if (words) textRef.current = words;
-  }, []);
-
   const handleGsapAnimation = useDebounce((timeline) => {
     timeline.play();
   }, 300);
+
+  useEffect(() => {
+    const text = new SplitType("div.text1");
+    const words = text[textStyle];
+    if (words) textRef.current = words;
+  }, [textStyle]);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
@@ -45,12 +60,12 @@ export default function Text() {
             opacity: gsapStates.opacityFrom,
           },
           {
-            x: gsapOptions.find((item) => item.type === "xTo")?.default,
-            y: gsapOptions.find((item) => item.type === "yTo")?.default,
+            x: gsapStates.xTo,
+            y: gsapStates.yTo,
             opacity: gsapStates.opacityTo,
             stagger: 0.1,
             duration: 0.5,
-            ease: "back",
+            ease: easing,
           }
         );
         timeline.pause();
@@ -60,7 +75,7 @@ export default function Text() {
     return () => {
       ctx.revert();
     };
-  }, [counter, gsapStates, handleGsapAnimation]);
+  }, [counter, gsapStates, handleGsapAnimation, textStyle, easing]);
 
   return (
     <div
@@ -79,7 +94,45 @@ export default function Text() {
           flexDirection: "column",
         }}
       >
-        <Button onClick={() => setCounter(counter + 1)}>refresh</Button>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "center",
+            gap: "1rem",
+          }}
+        >
+          <Select
+            defaultValue="words"
+            onChange={(_e, val: TextStyle | null) => val && setTextStyle(val)}
+          >
+            <Option value="chars">chars</Option>
+            <Option value="words">words</Option>
+            <Option value="lines">lines</Option>
+          </Select>
+          <Select
+            defaultValue="back"
+            onChange={(_e, val: EasingType | null) => val && setEasing(val)}
+          >
+            {[
+              "power0",
+              "power1",
+              "power2",
+              "power3",
+              "power4",
+              "back",
+              "elastic",
+              "bounce",
+              "rough",
+              "expo",
+            ].map((item) => (
+              <Option key={item} value={item}>
+                {item}
+              </Option>
+            ))}
+          </Select>
+          <Button onClick={() => setCounter(counter + 1)}>refresh</Button>
+        </div>
         <div style={{ height: 10 }} />
         {gsapOptions.map((item) => {
           if (item.componentType === "slider")
