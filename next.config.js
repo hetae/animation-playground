59c829adfd2648284224a56c919c4f0585ac3107
YeongGi <yeonggilol@gmail.com>
2023-05-21 19:19:23 +0900

    configure next export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  trailingSlash: true,
+  output: "export",
+};
+module.exports = nextConfig;
