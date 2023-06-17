364117cae74188630c11d302225f044f860af846
YeongGi <yeonggilol@gmail.com>
2023-06-17 12:35:48 +0900

    feat: add object type code gen situation

--- src/utils/convertObjectToStringWithTab.ts
+++ src/utils/convertObjectToStringWithTab.ts
@@ -1,10 +1,18 @@
-export const convertObjectToString = (
+export const convertObjectToString: any = (
   obj: { [key: string]: any },
   tabCount: number
 ) => {
   if (typeof obj !== "object") return "";
   return Object.entries(obj)
     .map(([key, value], index) => {
+      if (typeof value === "object") {
+        return `${"  ".repeat(tabCount)}${key}: {\n${convertObjectToString(
+          value,
+          tabCount + 1
+        )}\n${"  ".repeat(tabCount)}}${
+          index === Object.entries(obj).length - 1 ? "" : ","
+        }`;
+      }
       if (index === Object.entries(obj).length - 1) {
         return `${"  ".repeat(tabCount)}${key}: ${value},`;
       }
